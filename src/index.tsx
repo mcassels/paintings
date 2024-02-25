08dfb8a998b369b5c3cc23843221524aeb8be279
Morgan Brooks <morgan@urbanlogiq.com>
2024-02-25 11:41:36 -0800

    try to fix favicon

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Navigate, RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
   },
+  {
+    path: "*",
+    element: <Navigate to="/" replace />,
+  }
 ]);
 
 const root = ReactDOM.createRoot(
