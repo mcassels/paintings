9238324234782f676c849a560e4cd1cf0a656009
Morgan Brooks <morgancassels@gmail.com>
2024-04-11 08:28:44 -0700

    add-sentry

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  integrations: [
+    Sentry.replayIntegration(),
+  ],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
