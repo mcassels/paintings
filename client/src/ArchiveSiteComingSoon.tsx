adb846f1640a5a0c5850aad7a64187e1a81fda77
Morgan Brooks <morgancassels@gmail.com>
2025-10-22 17:47:30 -0700

    another landing page option

--- client/src/ArchiveSiteComingSoon.tsx
+++ client/src/ArchiveSiteComingSoon.tsx
@@ -6,31 +6,41 @@ import AppFooter from "./AppFooter";
 import ContactUsModal from "./ContactUsModal";
 
 
+function SectionHeader(props: { text: string }) {
+  return (
+    <div className="text-xl bg-white w-fit bg-opacity-75">
+      <div className="p-2">
+        {props.text}
+      </div>
+    </div>
+  );
+}
+
 export default function ArchiveSiteComingSoon() {
 
   // TODO: implement layout for mobile
   // const isMobile = getIsMobile();
 
   return (
-    <div className="min-h-svh flex flex-col items-stretch">
+    <div className="min-h-svh flex flex-col items-stretch archive-site-coming-soon-page">
       <ContactUsModal />
-      <Header className="bg-white flex flex-col text-4xl p-10 h-fit">
-        <div className="flex flex-col pl-20 font-light">
+      <Header className="bg-white flex flex-col text-4xl py-10 px-0 h-fit">
+        <div className="flex flex-col font-light px-4 lg:pl-20">
           James Gordaneer, RCA
         </div>
       </Header>
-      <div className="relative flex-1 min-h-0 pl-16 bg-cover bg-center"
-           style={{ backgroundImage: 'url(https://james-gordaneer-paintings.s3.ca-central-1.amazonaws.com/website_images/morning_stretch_1987.jpeg)'}}>
-        <div className="absolute inset-0 bg-white/50"></div>
+      <div className="flex-1 min-h-0 bg-cover bg-center px-4 lg:pl-20 lg:mx-0 mx-auto"
+           style={{ backgroundImage: 'url(https://james-gordaneer-paintings.s3.ca-central-1.amazonaws.com/website_images/pastel_blue_chair_painting.jpeg)'}}>
         <div className="py-6 space-y-6 flex-1">
+          <SectionHeader text="Upcoming projects" />
           <Card key="about-jim" style={{ width: "min(95%, 800px)"}}>
-            <div className="h-[300px]">
+            <div className="h-[200px]">
               <div className="h-full flex flex-shrink-0 space-x-4">
                 <Image
-                  title="jim-self-portrait"
-                  alt="jim-self-portrait"
+                  title="painting-of-armchair-by-window"
+                  alt="painting-of-armchair-by-window"
                   preview={false}
-                  src="https://james-gordaneer-paintings.s3.ca-central-1.amazonaws.com/website_images/jim-self-portrait.jpeg"
+                  src="https://james-gordaneer-paintings.s3.ca-central-1.amazonaws.com/website_images/armchair_window_painting.jpeg"
                   style={{
                     height: "100%",
                     width: "auto",
@@ -38,14 +48,15 @@ export default function ArchiveSiteComingSoon() {
                   }}
                 />
                 <div className="pt-2 p-10 pl-5">
-                  <div className="text-lg font-semibold pb-2">Digital archive coming soon</div>
-                  <div>Between 1950 and 2016, noted Canadian abstract expressionist James Gordaneer created thousands of paintings. This archive aims to showcase as many of those images as possible. Stay tuned for more.</div>
+                  <div className="text-lg font-semibold pb-2">Digital archive (coming soon!)</div>
+                  <div>Between 1950 and 2016, noted Canadian abstract expressionist James Gordaneer created thousands of paintings. This archive aims to document as many of these works as possible. Stay tuned for more.</div>
                 </div>
               </div>
             </div>
           </Card>
+          <SectionHeader text="Past projects" />
           <Card key="past-projects" style={{ width: "min(95%, 800px)"}}>
-            <div className="h-[180px]">
+            <div className="h-[200px]">
               <div className="h-full flex flex-shrink-0 space-x-4">
                 <Image
                   title="paintings-under-tarp"
@@ -60,9 +71,9 @@ export default function ArchiveSiteComingSoon() {
                 />
                 <div className="pt-2 p-10 pl-1 flex flex-col justify-between">
                   <div className="pl-4">
-                    <div className="text-lg font-semibold pb-2">Past projects</div>
+                    <div className="text-lg font-semibold pb-2">Painting adoption project</div>
                     <div>
-                      The James Gordaneer Painting Adoption project took place from April to May, 2024. More than 100 works were included.
+                      The James Gordaneer Painting Adoption project took place April to May, 2024. Over $XXXX was raised in support of the James Gordaneer Legacy Award, a scholarship given annually to a Camosun College Visual Arts student. More than 100 works were included.
                     </div>
                   </div>
                   <div>
