d0e0e4fa715cf29cd7fe05aab32184e391f02459
Morgan Brooks <morgancassels@gmail.com>
2025-11-12 07:55:05 -0800

    update landing page copy and zazzle link

--- client/src/ArchiveSiteComingSoon.tsx
+++ client/src/ArchiveSiteComingSoon.tsx
@@ -130,15 +130,15 @@ export default function ArchiveSiteComingSoon() {
           <div className="py-4">
             <Badge.Ribbon text="On sale now!" color="blue" placement="start">
               <ProjectCardResponsive
-                title="2026 Calendar and blank cards"
-                description="Support the James Gordaneer Legacy Award by purchasing a 2026 calendar or a set of greeting cards featuring the artwork of James Gordaneer."
+                title="2026 calendar and blank greeting cards"
+                description="Featuring the artwork of James Gordaneer, the 2026 Art of Victoria calendar supports the James Gordaneer Legacy Award. Blank greeting cards featuring some of Gordaneer’s iconic Victoria landscapes are perfect for any occasion."
                 imageUrl="https://james-gordaneer-paintings.s3.ca-central-1.amazonaws.com/website_images/calendar.webp"
                 imageAlt="calendar"
                 orientation="vertical"
                 extraContent={
                   <div className="">
                     <Button type="primary" ghost>
-                      <NavLink to="https://www.etsy.com/shop/jamesgordaneerart/" target="_blank">
+                      <NavLink to="https://www.zazzle.ca/store/jamesgordaneerart" target="_blank">
                         <div>
                           Browse the shop
                           <i className="fa-solid fa-up-right-from-square pl-2"></i>
@@ -152,14 +152,14 @@ export default function ArchiveSiteComingSoon() {
           </div>
           <SectionHeader text="Projects" />
           <ProjectCardResponsive
-            title="Digital archive (coming soon!)"
-            description="Between 1950 and 2016, noted Canadian abstract expressionist James Gordaneer created thousands of paintings. This archive aims to document as many of these works as possible. Stay tuned for more."
-            imageUrl="https://james-gordaneer-paintings.s3.ca-central-1.amazonaws.com/website_images/armchair_window_painting.webp"
+            title="Digital archive (Launching 2026)"
+            description="Between 1950 and 2016, noted Canadian abstract expressionist James Gordaneer created thousands of paintings. This digital archive aims to document as many of these works as possible, to share Gordaneer’s work with art lovers and students in Canada and around the world. Stay tuned for more."
+            imageUrl="https://james-gordaneer-paintings.s3.ca-central-1.amazonaws.com/website_images/paintings_archive.webp"
             imageAlt="painting-of-armchair-by-window"
           />
           <ProjectCardResponsive
             title="Painting adoption project"
-            description="The James Gordaneer Painting Adoption project took place April to May, 2024. The project raised funds in support of the James Gordaneer Legacy Award, a scholarship given annually to a Camosun College Visual Arts student. More than 100 works were included."
+            description="The James Gordaneer Painting Adoption project took place from April to May, 2024. The project raised funds in support of the James Gordaneer Legacy Award, a scholarship given annually by the Victoria Visual Artists Legacy Society to a Camosun College Visual Arts student. More than 100 works were included."
             imageUrl="https://james-gordaneer-paintings.s3.ca-central-1.amazonaws.com/website_images/paintings-under-tarp.webp"
             imageAlt="paintings-under-tarp"
             extraContent={
