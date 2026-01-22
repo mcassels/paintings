f18c219f36bf1b3e74987824b557ddd8b185db71
Morgan Brooks <morgancassels@gmail.com>
2026-01-21 22:19:20 -0800

    add newsletter signup form

--- client/src/ContactPage.tsx
+++ client/src/ContactPage.tsx
@@ -2,16 +2,18 @@ import ContactUsForm from "./ContactUsForm";
 
 export default function ContactPage() {
   return (
-    <div className="text-pretty p-4">
-      <div className="px-[10px]">
-        <div className="pb-2">
-          <h1 className="text-lg">Get in touch</h1>
-          <p className="max-w-[900px]">
-            If you have a James Gordaneer work, are interested in acquiring one, or
-            have any other questions, please send us a message.
-          </p>
+    <div className="flex-1 min-h-0 bg-cover bg-center px-4 lg:pl-20 lg:mx-0 mx-auto"
+    style={{ backgroundImage: 'url(https://james-gordaneer-paintings.s3.ca-central-1.amazonaws.com/website_images/pastel_blue_chair_painting.webp)'}}>
+      <div className="flex flex-col space-y-8 py-6">
+        <div className="max-w-[600px]">
+          <div style={{textAlign: "left"}} className="sender-form-field" data-sender-form-id="dyPxvW"></div>
         </div>
-        <div className="w-[800px] max-w-[95%]">
+        <div className="bg-white max-w-[800px]">
+          <div className="p-[24px]">
+            <h1 className="text-lg font-semibold py-1">Contact us</h1>
+            <p>If you have a James Gordaneer work, are interested in acquiring one, or
+            have any other questions, please send us a message.</p>
+          </div>
           <ContactUsForm />
         </div>
       </div>
